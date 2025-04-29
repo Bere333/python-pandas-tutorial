2211ad49cf47dc51dca37992ffb01cca86c92499
Berenice Ramos <elcosmossuena@gmail.com>
2025-04-29 22:36:44 +0000

    added exercises solution

---
+++ app.py
@@ -0,0 +1,22 @@
+# 02.1 Script with python
+# print("Hello World")
+
+# 02.2 Import
+import pandas as pd
+
+# data_frame = pd.read_csv(".learn/assets/pokemon_data.csv")
+#print(data_frame)
+
+# 04 Series
+# numbers_data = [23,45,7,34,6,63,36,78,54,34]
+# numbers_series = pd.Series(numbers_data)
+#print(numbers_series)
+
+# 04.1 Date Range
+pd_daterange = pd.date_range(start="2021-05-01", end="2021-05-12")
+print(pd_daterange)
+
+# 04.2 Series Apply
+
+
+
