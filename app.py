1f51bb295a72d7334875316ce10d0f269c58f894
Berenice Ramos <elcosmossuena@gmail.com>
2025-04-30 04:23:00 +0000

    added dataframe from dict

--- app.py
+++ app.py
@@ -13,10 +13,47 @@ import pandas as pd
 #print(numbers_series)
 
 # 04.1 Date Range
-pd_daterange = pd.date_range(start="2021-05-01", end="2021-05-12")
-print(pd_daterange)
+# pd_daterange = pd.date_range(start="2021-05-01", end="2021-05-12")
+# print(pd_daterange)
 
 # 04.2 Series Apply
+# my_series = pd.Series([2, 4, 6, 8, 10])
+# my_series_slices = my_series.apply(lambda x: x / 2)
+# print(my_series_slices)
+
+# 05 DataFrames
+# data = [["Toyota", "Corolla", "Blue"], ["Ford", "K", "Yellow"], ["Porsche", "Cayenne", "White"]]
+# df_cars = pd.DataFrame(data, columns=["Brand", "Model", "Color"])
+# print(df_cars)
+
+# 05.1 DataFrame Dict
+data = [
+    { 
+        "brand": "Toyota", 
+        "model": "Corolla",
+        "color": "Blue"
+    },
+    {
+        "brand": "Ford", 
+        "model": "K",
+        "color": "Yellow"
+    },
+    {
+        "brand": "Porsche", 
+        "model": "Cayenne",
+        "color": "White"
+    },
+    {
+        "brand": "Tesla",
+        "model": "Model S",
+        "color": "Red"
+    }
+]
+
+df_carsbrands = pd.DataFrame.from_dict(data)
+print(df_carsbrands)
+
+
 
 
 
