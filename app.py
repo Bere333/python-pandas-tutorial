6df5625fe5c06bfd1409f1e38eaa7465062458f7
Berenice Ramos <elcosmossuena@gmail.com>
2025-04-30 05:02:41 +0000

    added print column exercise solution

--- app.py
+++ app.py
@@ -27,31 +27,50 @@ import pandas as pd
 # print(df_cars)
 
 # 05.1 DataFrame Dict
-data = [
-    { 
-        "brand": "Toyota", 
-        "model": "Corolla",
-        "color": "Blue"
-    },
-    {
-        "brand": "Ford", 
-        "model": "K",
-        "color": "Yellow"
-    },
-    {
-        "brand": "Porsche", 
-        "model": "Cayenne",
-        "color": "White"
-    },
-    {
-        "brand": "Tesla",
-        "model": "Model S",
-        "color": "Red"
-    }
-]
+# data = [
+#     { 
+#         "brand": "Toyota", 
+#         "model": "Corolla",
+#         "color": "Blue"
+#     },
+#     {
+#         "brand": "Ford", 
+#         "model": "K",
+#         "color": "Yellow"
+#     },
+#     {
+#         "brand": "Porsche", 
+#         "model": "Cayenne",
+#         "color": "White"
+#     },
+#     {
+#         "brand": "Tesla",
+#         "model": "Model S",
+#         "color": "Red"
+#     }
+# ]
 
-df_carsbrands = pd.DataFrame.from_dict(data)
-print(df_carsbrands)
+# df_carsbrands = pd.DataFrame.from_dict(data)
+# print(df_carsbrands)
+
+# 05.2 DataFrame iLoc
+df_poke = pd.read_csv(".learn/assets/pokemon_data.csv")
+# df_cells = df_poke.iloc[133, 6]
+# print(df_cells)
+
+# 05.3 DataFrame Head
+# df_head = df_poke.head(3)
+# print(df_head)
+
+# 05.4 DataFrame Tail
+# df_tail = df_poke.tail(3)
+# print(df_tail)
+
+# 05.5 Print Columns
+df_columns = df_poke[["Name", "Type 1"]][0:10]
+print(df_columns)
+
+
 
 
 
